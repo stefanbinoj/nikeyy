da2563dc40f2e9241679c706e82ad2bf27c499c9
stefan <stefan.binoj.007@gmail.com>
2024-09-16 11:21:22 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+const App = () =>(
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      Popular Products
+    </section>
+    <section className="padding">
+      Super quality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      Special Offers
+    </section>
+    <section className="bg-pale-blue padding">
+      Customer Review
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="bg">
+      Footer
+    </section>
+
+  </main>
+)
+
+export default App;
